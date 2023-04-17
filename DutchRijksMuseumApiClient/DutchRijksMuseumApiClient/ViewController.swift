cd62c298b992258b5cc69584982acba76479c495
Alex Fernandez <alejandro.fernandez@fillthegapp.com>
2023-04-17 09:31:07 +0200

    Creation of the projecto and basic files

---
+++ DutchRijksMuseumApiClient/DutchRijksMuseumApiClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DutchRijksMuseumApiClient
+//
+//  Created by Alejandro Fernandez Ruiz on 17/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
